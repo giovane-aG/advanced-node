e927749f5e487396d61a1ad3979797af0b67790f
Giovane <giovaneaalmeida27@gmail.com>
2022-11-09 16:35:28 -0300

    refact: create LoadFacebookUser interface

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -2,22 +2,47 @@ import { describe, it, expect } from 'vitest'
 import { AuthenticationError } from '../../../src/domain/errors'
 import { FacebookAuthentication } from '../../../src/domain/features'
 
+interface LoadFacebookUser {
+  loadUser: (params: LoadFacebookUser.Params) => Promise<void>
+}
+
+class LoadFacebookUserApiSpy implements LoadFacebookUser {
+  token?: string
+  result = { name: 'any_name' }
+
+  async loadUser (params: LoadFacebookUser.Params): Promise<void> {
+    this.token = params.token
+  }
+}
+
+namespace LoadFacebookUser {
+  export type Params = {
+    token: string
+  }
+}
+
 class FacebookAuthenticationService {
-  public token: string
+
+  constructor (
+    private readonly loadFacebookUserByTokenApi: LoadFacebookUser
+  ) {}
 
   async perform(params: FacebookAuthentication.Params): Promise<FacebookAuthentication.Params> {
-    this.token = params.token
     if (!params.token || params.token == '') throw new AuthenticationError()
-
+    await this.loadFacebookUserByTokenApi.loadUser(params)
     return params
   }
 }
 
 describe('Facebook Authentication', () => {
+
   it('should have the same token as the one provided', async () => {
-    const sut = new FacebookAuthenticationService()
+    const loadFacebookUserApiSpy = new LoadFacebookUserApiSpy()
+    const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy)
+
     await sut.perform({ token: 'any_token' })
-    expect(sut.token).toBe('any_token')
+
+    expect(loadFacebookUserApiSpy.token).toBe('any_token')
   })
 
   it('should throw an AuthenticationError when a token is not provided', async () => {
