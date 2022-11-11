012117f0d98f03c4cc4e43946db0902e39796645
Giovane <giovaneaalmeida27@gmail.com>
2022-11-11 15:39:36 -0300

    format code

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,62 +1,79 @@
-import { describe, it, expect, vitest, vi, beforeEach } from 'vitest'
-import { AuthenticationError } from '../../../src/domain/errors'
-import { FacebookAuthentication } from '../../../src/domain/features'
-import { LoadFacebookUserApi } from '../../../src/data/contracts/apis'
-import { CreateUserAccountRepository, LoadUserAccountRepository } from '../../../src/data/contracts/apis/repos'
-import { FacebookAuthenticationService } from '../../../src/data/services'
+import { describe, it, expect, vitest, vi, beforeEach } from "vitest";
+import { AuthenticationError } from "../../../src/domain/errors";
+import { FacebookAuthentication } from "../../../src/domain/features";
+import { LoadFacebookUserApi } from "../../../src/data/contracts/apis";
+import {
+  CreateFacebookAccountRepository,
+  LoadUserAccountRepository,
+} from "../../../src/data/contracts/apis/repos";
+import { FacebookAuthenticationService } from "../../../src/data/services";
 
-describe('Facebook Authentication', () => {
-
-  let loadFacebookUserApiSpy: LoadFacebookUserApi
-  let sut: FacebookAuthenticationService
-  let loadUserAccountRepoSpy: LoadUserAccountRepository
-  let createUserAccountRepoSpy: CreateUserAccountRepository
+describe("Facebook Authentication", () => {
+  let loadFacebookUserApiSpy: LoadFacebookUserApi;
+  let sut: FacebookAuthenticationService;
+  let loadUserAccountRepoSpy: LoadUserAccountRepository;
+  let createFacebookAccountRepoSpy: CreateFacebookAccountRepository;
 
   beforeEach(() => {
-    loadFacebookUserApiSpy   = { loadUser: vi.fn() }
-    loadUserAccountRepoSpy   = { loadUser: vi.fn() }
-    createUserAccountRepoSpy = { create: vi.fn() }
-    sut = new FacebookAuthenticationService(loadFacebookUserApiSpy, loadUserAccountRepoSpy, createUserAccountRepoSpy)
+    loadFacebookUserApiSpy = { loadUser: vi.fn() };
+    loadUserAccountRepoSpy = { loadUser: vi.fn() };
+    createFacebookAccountRepoSpy = { createFromFacebook: vi.fn() };
+    sut = new FacebookAuthenticationService(
+      loadFacebookUserApiSpy,
+      loadUserAccountRepoSpy,
+      createFacebookAccountRepoSpy
+    );
 
     loadFacebookUserApiSpy.loadUser = vi.fn().mockResolvedValue({
-      name: 'any_name',
-      email: 'any_email',
-      facebookId: 'any_facebook_id'
-    })
-  })
+      name: "any_name",
+      email: "any_email",
+      facebookId: "any_facebook_id",
+    });
+  });
 
-  it('should have the same token as the one provided', async () => {
-    vitest.spyOn(loadFacebookUserApiSpy, 'loadUser')
+  it("should have the same token as the one provided", async () => {
+    vitest.spyOn(loadFacebookUserApiSpy, "loadUser");
 
-    await sut.perform({ token: 'any_token' })
+    await sut.perform({ token: "any_token" });
 
-    expect(loadFacebookUserApiSpy.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
-    expect(loadFacebookUserApiSpy.loadUser).toHaveBeenCalledTimes(1)
-  })
+    expect(loadFacebookUserApiSpy.loadUser).toHaveBeenCalledWith({
+      token: "any_token",
+    });
+    expect(loadFacebookUserApiSpy.loadUser).toHaveBeenCalledTimes(1);
+  });
 
-  it('should throw an AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
-    vi.spyOn(loadFacebookUserApiSpy, 'loadUser').mockResolvedValueOnce(undefined)
-    const authResult = await sut.perform({ token: 'any_token' })
+  it("should throw an AuthenticationError when LoadFacebookUserApi returns undefined", async () => {
+    vi.spyOn(loadFacebookUserApiSpy, "loadUser").mockResolvedValueOnce(
+      undefined
+    );
+    const authResult = await sut.perform({ token: "any_token" });
 
-    expect(authResult).toBeInstanceOf(AuthenticationError)
-  })
+    expect(authResult).toBeInstanceOf(AuthenticationError);
+  });
 
-  it('should call LoadUserAccountRepository when LoadFacebookUserApi returns data', async () => {
-    await sut.perform({ token: 'any_token' })
-    expect(loadUserAccountRepoSpy.loadUser).toHaveBeenCalledOnce()
-    expect(loadUserAccountRepoSpy.loadUser).toHaveBeenCalledWith({ email: 'any_email'})
-  })
+  it("should call LoadUserAccountRepository when LoadFacebookUserApi returns data", async () => {
+    await sut.perform({ token: "any_token" });
+    expect(loadUserAccountRepoSpy.loadUser).toHaveBeenCalledOnce();
+    expect(loadUserAccountRepoSpy.loadUser).toHaveBeenCalledWith({
+      email: "any_email",
+    });
+  });
 
-  it('should call CreateUserAccountRepo when LoadUserAccountRepo returns data', async () => {
-    vi.spyOn(loadUserAccountRepoSpy, 'loadUser').mockResolvedValueOnce(undefined)
-    await sut.perform({ token: 'any_token'})
+  it("should call CreateUserAccountRepo when LoadUserAccountRepo returns data", async () => {
+    vi.spyOn(loadUserAccountRepoSpy, "loadUser").mockResolvedValueOnce(
+      undefined
+    );
+    await sut.perform({ token: "any_token" });
 
-    expect(createUserAccountRepoSpy.create).toHaveBeenCalledWith({
-      name: 'any_name',
-      email: 'any_email',
-      facebookId: 'any_facebook_id'
-    })
-    expect(createUserAccountRepoSpy.create).toHaveBeenCalledOnce()
-  })
-
-})
+    expect(
+      createFacebookAccountRepoSpy.createFromFacebook
+    ).toHaveBeenCalledWith({
+      name: "any_name",
+      email: "any_email",
+      facebookId: "any_facebook_id",
+    });
+    expect(
+      createFacebookAccountRepoSpy.createFromFacebook
+    ).toHaveBeenCalledOnce();
+  });
+});
